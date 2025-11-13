107623e8d71e8d73ba66fd2b68c6650722badfec
DTS4You <info@datentechnikschwarz.de>
2025-11-13 23:20:11 +0100

    Init

--- libs/module_xio.py
+++ libs/module_xio.py
@@ -7,7 +7,32 @@ from machine import Pin, PWM        # type: ignore
 #from utime import sleep             # type: ignore
 
 
+class XIO:
 
+    def __init__(self, dir):
+        self.dir = dir
+        self.pin_n = []
+        if dir:
+            self.set_xio_out()
+        else:
+            self.set_xio_in()
+
+    def set_xio_out(self):
+        self.pin_n.append(Pin(10, mode=Pin.OUT))
+        self.pin_n.append(Pin(11, mode=Pin.OUT))
+        self.pin_n.append(Pin(12, mode=Pin.OUT))
+        self.pin_n.append(Pin(13, mode=Pin.OUT))
+
+    def set_xio_in(self):
+        self.pin_n[0] = Pin(10, mode=Pin.IN, pull=Pin.PULL_UP)
+        self.pin_n[1] = Pin(11, mode=Pin.IN, pull=Pin.PULL_UP)
+        self.pin_n[2] = Pin(12, mode=Pin.IN, pull=Pin.PULL_UP)
+        self.pin_n[3] = Pin(13, mode=Pin.IN, pull=Pin.PULL_UP)
+    
+    def set_out(self):
+        self.pin_n[0].value(1)
+        self.pin_n[1].value(0)
+    
 # -----------------------------------------------------------------------------
 def main():
 
@@ -15,6 +40,11 @@ def main():
 
     try:
         print("Start")
+
+        xio = XIO(1)
+
+        xio.set_out()
+
     except KeyboardInterrupt:
         print("Keyboard Interrupt")
     finally:
