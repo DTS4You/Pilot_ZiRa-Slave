979d77d8d9332ea518efbb7b051d755d62c2c9a8
DTS4You <info@datentechnikschwarz.de>
2025-11-25 11:25:20 +0100

    Init

--- libs/module_init.py
+++ libs/module_init.py
@@ -66,8 +66,8 @@ class Global_WS2812:
     color_half          = (110,  2,  2)
     color_blink_on      = (255, 20, 20)
     color_blink_off     = (  0,  0,  5)
-    color_red           = (255,255,  0)
-    color_green         = (  0,255,255)
+    color_red           = ( 70,  0,  0)
+    color_green         = (  0, 70,  0)
 
 
 class Global_Default:
